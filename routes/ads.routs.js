9acab2f7ce011557ee06263b2acf692bf7e357a9
Illia Stadnyk <ilya.stadnyk@gmail.com>
2025-07-03 22:15:01 +0200

    Implemented Ad system

--- routes/ads.routs.js
+++ routes/ads.routs.js
@@ -1,16 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const authMiddleware = require("../utils/authMiddleware");
-const multer = require("multer");
-const ad = require('../controllers/ads.controller')
+const authMiddleware = require('../utils/authMiddleware');
+const multer = require('multer');
+const ad = require('../controllers/ads.controller');
 const uploadFile = require('../middlewares/imageUploadWithErrorHandling');
 
-
-router.get("/ads", authMiddleware, ad.getAllAds)
-router.get("/ads/:id", authMiddleware, ad.getAd)
-router.post("/ads", authMiddleware, uploadFile('image'), ad.addAd)
-router.delete("/ads/:id", authMiddleware, ad.delete)
-router.put("/ads/:id", authMiddleware, uploadFile('image'), ad.updateAd)
-router.get("/ads/search/:searchPhrase", authMiddleware, ad.searchAds)
+router.get('/ads', ad.getAllAds);
+router.get('/ads/:id', ad.getAd);
+router.post('/ads', authMiddleware, uploadFile('image'), ad.addAd);
+router.delete('/ads/:id', authMiddleware, ad.delete);
+router.patch('/ads/:id', authMiddleware, uploadFile('image'), ad.updateAd);
+router.get('/ads/search/:searchPhrase', ad.searchAds);
 
 module.exports = router;
